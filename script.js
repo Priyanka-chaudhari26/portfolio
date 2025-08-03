25f1f2c8816dc1248863a10b042b94dd90f6f586
Priyanka-chaudhari26 <priyankapchaudhari26@gmail.com>
2025-08-03 17:33:48 +0530

    fixed navbar on small screens

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const navItems = document.querySelectorAll(".nav-links a");
 
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
 });
 
-
-  const words = ["Web Developer", "Designer", "Problem Solver"];
-  let wordIndex = 0;
-  let charIndex = 0;
-  const typingText = document.querySelector(".typing-text");
-
-  function typeWord() {
-    if (charIndex < words[wordIndex].length) {
-      typingText.textContent += words[wordIndex].charAt(charIndex);
-      charIndex++;
-      setTimeout(typeWord, 100); // typing speed
-    } else {
-      setTimeout(eraseWord, 2000); // pause before erasing
+navItems.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (window.innerWidth <= 768) {
+      navLinks.classList.remove("show");
     }
-  }
-
-  function eraseWord() {
-    if (charIndex > 0) {
-      typingText.textContent = words[wordIndex].substring(0, charIndex - 1);
-      charIndex--;
-      setTimeout(eraseWord, 50); // erasing speed
-    } else {
-      wordIndex = (wordIndex + 1) % words.length; // next word
-      setTimeout(typeWord, 500);
-    }
-  }
-
-  document.addEventListener("DOMContentLoaded", function() {
-    typeWord();
   });
+});
 
+const words = ["Web Developer", "Designer", "Problem Solver"];
+let wordIndex = 0;
+let charIndex = 0;
+const typingText = document.querySelector(".typing-text");
+
+function typeWord() {
+  if (charIndex < words[wordIndex].length) {
+    typingText.textContent += words[wordIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeWord, 100); // typing speed
+  } else {
+    setTimeout(eraseWord, 2000); // pause before erasing
+  }
+}
+
+function eraseWord() {
+  if (charIndex > 0) {
+    typingText.textContent = words[wordIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseWord, 50); // erasing speed
+  } else {
+    wordIndex = (wordIndex + 1) % words.length; // next word
+    setTimeout(typeWord, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  typeWord();
+});
